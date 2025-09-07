b12e89f74d275a60d4e23d20d7e2c2eb09c992d5
Sushrut Rai <sushrutrai99@gmail.com>
2025-09-07 08:14:00 +0545

    DBDB

---
+++ deleteCookie.php
@@ -0,0 +1,9 @@
+<?php
+    setcookie("user","Hira",time()+ 3600,"/");
+    echo"Cookie deleted.";
+    if(count($_COOKIE)>0){
+        echo"cookies are enabled";
+    }else{
+        echo"cookies are disabled";
+    }
+?>
