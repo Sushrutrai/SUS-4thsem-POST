2f9093bc310cb8186ba09199ab8f969e47f1f1f5
Sushrutrai <sushrutrai99@gmail.com>
2025-12-18 10:51:58 +0545

    event handling samma Windows and Frame che baki cha

---
+++ php/preBoard.php
@@ -0,0 +1,45 @@
+<?php
+$x=10;
+$y=10.00;
+$arr=[1,2,3];
+$str="hell";
+$str2="hell";
+echo"fuck";
+print($arr[1]);
+printf('hello %d',$arr[2]);
+array_push($arr,22);
+array_shift($arr);
+print_r($arr);
+echo"$str$str2";
+echo"learn"."now";
+print $x;
+function add($a,$b){
+$mul=$a*$b;
+echo $mul;
+}
+$player=[
+1=>'ram',2=>'sures'
+];
+
+ADD(10,2,23434,232323);
+$word=array('a','b','c');
+next($word);
+prev($word);
+
+echo (end($player));
+echo (next($word));
+echo is_array($arr);
+array_unshift($arr,67);
+array_flip($arr);
+print_r($arr);
+echo(trim("this is wonderful"));
+echo strpos("hell man","mn");
+echo htmlentities('<h1>huh</h1>');
+echo (1!==2);
+
+class PreBoard{
+
+}
+class_exists('PreBoard');
+$date=new Preboard(); 
+?>
